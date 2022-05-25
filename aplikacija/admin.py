a5dfa94474b8c534f3fa6606643370aee2d34d8e
Antonija <antonijapavic@ssklivno.net>
2022-05-25 08:56:41 +0200

    8.commit-admin.py

--- aplikacija/admin.py
+++ aplikacija/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Eksterijer, Posts
+from .models import Interijer
+from .models import Eksterijer
+from .models import Detail
 # Register your models here.
+
+class PostsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class InterijerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+class EksterijerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+class DetailAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Posts, PostsAdmin)
+admin.site.register(Interijer, InterijerAdmin)
+admin.site.register(Eksterijer,EksterijerAdmin)
+admin.site.register(Detail,DetailAdmin)
